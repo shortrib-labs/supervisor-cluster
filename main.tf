7b785f32a2bfdf6838fe34d0ef58ea1dbd80e847
Chuck D'Antonio <cdantonio@pivotal.io>
2020-11-23 12:05:47 -0500

    Fixes error with missing datastore object (#3)
    TL;DR
    -----
    
    Adds missing Terraform object required for content libraries
    
    Details
    -------
    
    The merge that added content libraries was missing a change that
    added a data source for the vSAN datastore. This commit adds the
    file so that things will work.

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ data "vsphere_compute_cluster" "homelab" {
   name = "homelab"
   datacenter_id = data.vsphere_datacenter.garage.id
 }
+
+data "vsphere_datastore" "vsan" {
+  name          = "vsanDatastore"
+  datacenter_id = data.vsphere_datacenter.garage.id
+}
